d1d76ab4b94bf933f3f42756f01abd5bb0296237
ShaunAUS <wnsqud70@gmail.com>
2024-12-17 19:06:54 +0900

    synchronized

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -15,10 +15,23 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
+
+    //synchronized + Transactional 트랜잭션 끝날떄쯤에 (db업데이트전)에 다른 스레드가 자원접근
+    //synchronized 각 프로세스 안에서만 보장 , 즉 여러 쓰레드에서 동시에 접근하면 레이스컨디션 발생
+/*
     @Transactional
-    public void decrease(Long id, Long quantity){
+    public synchronized void decrease(Long id, Long quantity){
         Stock stock = stockRepository.findById(id).orElseThrow();
         stock.decrease(quantity);
         stockRepository.save(stock);
     }
+*/
+
+    @Transactional
+    public  void decrease(Long id, Long quantity){
+        Stock stock = stockRepository.findById(id).orElseThrow();
+        stock.decrease(quantity);
+        stockRepository.save(stock);
+    }
+
 }
